7f3b46bc97ce2a42e2e4071e7ec8a2137b884b0d
nickdxt <dellabetanicoly@gmail.com>
2024-11-22 19:43:06 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,28 @@
+import requests
+import streamlit as st
+
+def buscar_letra(banda,musica):
+    endpoint = f"https://api.lyrics.ovh/v1/{banda}/{musica}"
+    response = requests.get(endpoint)
+    letra = response.json()["lyrics"] if response.status_code == 200 else ""
+    return letra
+
+
+st.image("https://img.freepik.com/premium-photo/cat-singer-ai-generated_268835-4331.jpg")
+st.title("Letras de músicas")
+
+banda = st.text_input("Digite o nome da banda: ", key="banda")
+musica = st.text_input("Digite o nome da música: ", key="musica")
+pesquisar = st.button("Pesquisar")
+
+if pesquisar:
+    letra = buscar_letra(banda, musica)
+    if letra:
+        st.success("Encontramos a letra da sua música!")
+        st.text(letra)
+    else:
+        st.error("Não foi possível encontrar a letra dessa música")
+
+
+
+
